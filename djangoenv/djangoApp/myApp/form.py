7b07d0f36be2177753c071ea51ff00ebb33026ef
Balram <singhbalram054@gmail.com>
2019-10-23 00:14:01 +0530

    model form

---
+++ djangoenv/djangoApp/myApp/form.py
@@ -0,0 +1,7 @@
+from django import forms
+from myApp.models import Student
+
+class stuForm(forms.ModelForm):
+    class Meta:
+        model= Student
+        fields = '__all__'
--- djangoenv/djangoApp/myApp/views.py
+++ djangoenv/djangoApp/myApp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from myApp.form import stuForm
 # Create your views here.
 
+'''
 def home(request):
     return render(request, 'home.html')
+'''
+
+def home(request):
+    stu = stuForm()
+    return render(request, 'home.html', {'form':stu})
