917cd7863bfe13684f0e21be0836cd64b8ca08b6
Balram <singhbalram054@gmail.com>
2019-11-05 19:21:06 +0530

    set session

--- crud_example/employee/views.py
+++ crud_example/employee/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def setsession(request):
+    request.session['sname']='balram'
+    request.session['semail']= 'singhbalram54@gmail.com'
+    return HttpResponse('session is set')
