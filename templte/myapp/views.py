2304f01a13c5f7dcf845ce07b0bd021564551876
Balram <singhbalram054@gmail.com>
2019-11-08 22:14:24 +0530

    template date time fielf

--- templte/myapp/views.py
+++ templte/myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    mytime = datetime.now()
+ #   context={'time':mytime}
+    return render(request, 'home.html',{'mytime':mytime})
+    #return render(request, 'home.html',context)
