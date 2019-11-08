ead852635dd6f4f725120288b015efdd0a218a8c
Balram <singhbalram054@gmail.com>
2019-11-08 23:33:47 +0530

    if tag in django template

--- templte/myapp/views.py
+++ templte/myapp/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from datetime import datetime
+import datetime
 # Create your views here.
 
 
 def home(request):
-    mytime = datetime.now()
- #   context={'time':mytime}
+    mytime = datetime.datetime.now().date()
     return render(request, 'home.html',{'mytime':mytime})
-    #return render(request, 'home.html',context)
